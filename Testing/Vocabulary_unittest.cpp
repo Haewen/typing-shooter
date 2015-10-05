d65450aee67eeb3a5b03f14ad82bad45496b6797
Hajas <hajos77@gmail.com>
2015-10-05 20:38:13 +0200

    Added documentation

--- Testing/Vocabulary_unittest.cpp
+++
@@ -1,9 +0,0 @@
-#include <gtest/gtest.h>
-#include "Vocabulary.h"
-
-TEST(Vocabulary, getRandomWord) {
-    Vocabulary* vocabulary = new Vocabulary("Words/en");
-    std::string text = vocabulary->getRandomWord(Difficulty::Hard);
-    std::cout << "[          ] random test word = " << text << std::endl;
-    EXPECT_GT(text.length(), 0);
-}
